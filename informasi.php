b22b978dc948b3160c8ef3c0463d675dd851b743
brilian12 <brilianfaqih354@gmail.com>
2022-09-22 16:24:02 +0700

    baru

--- informasi.php
+++ informasi.php
@@ -1,5 +1,8 @@
 <?php
 echo "<h2>Dzikri Faza Hauna</h2>";
-echo "2005008";
-echo "dzikrifaza15@gmail.com";
+echo "2005008<br>";
+echo "dzikrifaza15@gmail.com<br>";
+echo "<h2>Brlian Faqih</h2>";
+echo "2005006<br>";
+echo "brilianfaqih354@gmail.com";
 ?>
